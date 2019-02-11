92b2ac070655389578c555d0d096a0e504a4da79
vina <vina.chang@shopee.com>
2019-02-11 23:17:45 +0800

    implement parser_transition

--- a3/parser_transitions.py
+++ a3/parser_transitions.py
@@ -30,8 +30,9 @@ class PartialParse(object):
         ###
         ### Note: The root token should be represented with the string "ROOT"
         ###
-
-
+        self.stack = ['ROOT']
+        self.buffer = sentence.copy()
+        self.dependencies = []
         ### END YOUR CODE
 
 
@@ -49,7 +50,15 @@ class PartialParse(object):
         ###         1. Shift
         ###         2. Left Arc
         ###         3. Right Arc
-
+        if transition == 'S':
+            self.stack.append(self.buffer[0])
+            self.buffer = self.buffer[1:]
+        elif transition == 'LA':
+            self.dependencies.append((self.stack[-1], self.stack[-2]))
+            self.stack[-2] = self.stack[-1]
+            self.stack.pop()
+        elif transition == 'RA':
+            self.dependencies.append((self.stack[-2], self.stack.pop()))
 
         ### END YOUR CODE
 
@@ -100,10 +109,17 @@ def minibatch_parse(sentences, model, batch_size):
     ###             contains references to the same objects. Thus, you should NOT use the `del` operator
     ###             to remove objects from the `unfinished_parses` list. This will free the underlying memory that
     ###             is being accessed by `partial_parses` and may cause your code to crash.
-
-
+    partial_parses = [PartialParse(s) for s in sentences]
+    unfinished_parses = partial_parses[:]
+    while len(unfinished_parses) > 0:
+        minibatch = unfinished_parses[:batch_size]
+        predictions = model.predict(minibatch)
+        for i, (parse, pred) in enumerate(zip(minibatch, predictions)):
+            parse.parse_step(pred)
+            if len(parse.buffer) == 0 and len(parse.stack) == 1:
+                unfinished_parses.pop(i)
+    dependencies = [p.dependencies for p in partial_parses]
     ### END YOUR CODE
-
     return dependencies
 
 
